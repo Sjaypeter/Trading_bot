75014337c55f997b79da7732f34fb1cdc4d6e36a
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-19 12:24:58 +0100

    Interface to add buttons to control bot

--- bot.py
+++ bot.py
@@ -41,6 +41,9 @@ class TradingBotGUI:
         self.symbol_entry = tk.Entry(self.form_frame)
         self.symbol_entry.grid(row=1, column=5)
 
+        self.add_button = tk.Button(self.form_frame, text="Add Equity", command=self.add_equity)
+        self.add_button.grid(row=0, column=6)
+
         #Table to track the traded equities
         self.tree = ttk.Treeview(root, columns=("Symbol", "Position", "Entry Price", "Levels", "status"))
         for col in ["Symbol", "Position", "Entry Price", "Levels", "Status"]:
@@ -49,5 +52,12 @@ class TradingBotGUI:
         self.tree.pack(pady=10)
 
 
-        #
+        #Buttons to control the bot
+        self.toggle_system_button = tk.Button(root, text="Toggle Selected System", command=self.toggle_system)
+        self.toggle_system_button.pack(pady=5)
+
+        self.remove_button = tk.Button(root, text="Remove Selected Equity", command=self.remove_selected_equity)
+        self.remove_button.pack(pady=5)
+
+        
 
